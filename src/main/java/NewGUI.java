9135eec52971aee30a271addec74047880fcbfd8
Vlad1235 <fn1235@bk.ru>
2019-11-05 12:00:31 +0900

    workingPasswordGenerator

--- src/main/java/NewGUI.java
+++ src/main/java/NewGUI.java
@@ -1,20 +1,28 @@
 import java.awt.*;
 import java.awt.event.*;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
-public class NewGUI extends Frame implements ActionListener {
+public class NewGUI extends Frame implements ActionListener,WindowListener {
 
     private Label descPasswordLengh;
     private Label descPasswordResult;
     private TextField passwLength;
     private TextField passwFinal;
-    private int number = 0;
 
     public NewGUI(){
         setLayout(new FlowLayout());
         setTitle("Password Generator");
         setSize(300,100);
+
+        Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(),
+                fSize = super.getSize();
+        if (fSize.height > sSize.height) {
+            fSize.height = sSize.height;
+        }
+        if (fSize.width > sSize.width) {
+            fSize.width = sSize.width;
+        }
+        super.setLocation((sSize.width - fSize.width) / 2,
+                (sSize.height - fSize.height) / 2);
 
 
         descPasswordLengh = new Label("Password length: ");
@@ -22,15 +30,16 @@ public class NewGUI extends Frame implements ActionListener {
         passwLength = new TextField(5);
         passwLength.setEditable(true);
         add(passwLength);
+        passwLength.addActionListener(this);
 
         descPasswordResult = new Label("Password is: ");
         add(descPasswordResult);
         passwFinal = new TextField(10);
         passwFinal.setEditable(false);
         add(passwFinal);
-        passwFinal.addActionListener(this::actionPerformed);
 
         setVisible(true);
+        addWindowListener(this);
     }
 
     @Override
@@ -42,8 +51,36 @@ public class NewGUI extends Frame implements ActionListener {
     }
 
 
-
     public static void main(String[] args) {
-        NewGUI newGUI = new NewGUI();
+         new NewGUI();
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        super.dispose();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
     }
 }
--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,18 +1,18 @@
-
-import java.util.Scanner;
-/**
- * Password Generator
- */
-public class Output {
-    public static void main(String[] args) {
-        int x;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input password length: ");
-        x = scanner.nextInt();
-        Input input = new Input(x);
-        System.out.println();
-        System.out.println("your password is: \n");
-        System.out.println(input.getCount_value());
-    }
-}
-
+//
+//import java.util.Scanner;
+///**
+// * Password Generator
+// */
+//public class Output {
+//    public static void main(String[] args) {
+//        int x;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input password length: ");
+//        x = scanner.nextInt();
+//        Input input = new Input(x);
+//        System.out.println();
+//        System.out.println("your password is: \n");
+//        System.out.println(input.getCount_value());
+//    }
+//}
+//
